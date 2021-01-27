3450e2726701b5df002178a0d01fa1bda44f5d43
Michelle DePrenger-Levin <DePrengM@BotanicGardens.org>
2021-01-27 15:58:32 -0700

    parameters for fast slow itero semel and normal distribution of lambdas

--- R/ConstrainedVirtualSpeciesTypes.R
+++ R/ConstrainedVirtualSpeciesTypes.R
@@ -1,5 +1,3 @@
-# install.packages("PACLasso")
-# library(PACLasso)
 library(popbio)
 library(ggplot2)
 library(dplyr)
@@ -19,48 +17,37 @@ survivorship <- function(alpha1, x, alpha3, beta3){
   out
 }
 
-survivorship(.5,.3,.4,.5)
-
+# just to visualize hazard changes over stage
 hazard <- function(alpha2, beta2, x){
   alpha2 * exp(beta2*x)
 }
 
 
-# <https://rushinglab.github.io/WILD3810/articles/lab7_tree_harvest.html> 
-eigen(t_ij)
-# a negative value flips space, inverts space, absolute value is how scaled, first derivative is the 
-# determinant is the volume, columns are linearly dependent if squishes to a plane, line, or point
-# thumb = khat, pointer = ihat, middle = jhat
-det(t_ij) # A - lambda*I where roots of polynomial are the eigenvalues, how much the linear transformation scales the unit area, 
-# This makes it very small, if goes onto a line or point, all regions becomes zero, into a smaller dimension
- solve(t_ij) # The inverse, so multiplying by the matirx and by the inverse gets back to original 
-vec <- matrix(runif(4, 0,1), ncol = 1)
-solve(t_ij) %*% (t_ij %*% vec)
-solve(t_ij) %*% t_ij
-
 # ------------------------------------------------ constrain lambda ---------------------------
-# functions
-fecunditysurvival <- function(s, errorSD = 0.1){ 
+# functions 
+# Takada and Kawai 2020; s would be s_44
+fecunditysurvival <- function(s, errorSD = 0.01){ 
   out <- -1.25*((s+4.6)^2) + 36.45 + rnorm(1, 0 ,errorSD)  
   out
 }
 
-# or fecundity from Fujiwara and Diaz-Lopez 2017: b(x)
+# or fecundity from Fujiwara and Diaz-Lopez 2017: b(x); NO - only have one stage class that is reproductive, not applicable
 fecundityProportionalSize <- function(R, L_inf, kapa, x){
   R*(L_inf * (1-exp(-kapa*x))^3)
 }
 
-# Fujiwara and Diaz-Lopez 2017
+# Fujiwara and Diaz-Lopez 2017 
+# Do the x's represent the median age of the stage class???
 survivalTypeIII <- function(alpha1,x,alpha3,beta3){
   #Hazard
   # h(x) <- alpha3*exp((-beta3 * x))
   exp(-alpha1*x) * exp((alpha3/beta3)*(1-exp(beta3 * x)))
 }
 
-# Constant Hazard and Increasing fecundity
-surv <- survivalTypeIII(0.5, 1:4, 0,0.4)
-plot(1:4,surv, type="l")
-abline(lm(surv ~ c(1:4)), col="red")
+survivalTypeIII(alpha1 = 0.01, seq(1.5,4.5), alpha3 = 0.04, beta3 = 0.3)
+survivalTypeIII(alpha1 = 0.01, c(1.5,8.5,10.5,40.5), alpha3 = 0.04, beta3 = 0.3)
+survivalTypeIII(alpha1 = 0.01, c(1.5,4.5,8.5,12.5), alpha3 = 0.04, beta3 = 0.3) # slow? longer lived? 
+
 
 # Visualize what the survivalTypeIII equation is doing
 toplot <- do.call(rbind, lapply(seq(0.1,0.9,by=0.1), function(a1){
@@ -75,90 +62,122 @@ toplot <- do.call(rbind, lapply(seq(0.1,0.9,by=0.1), function(a1){
  }))
 head(toplot)
 #
-ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParameters.jpg",
-       ggplot(toplot, aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
-         geom_point()+
-         geom_line()+
-         facet_wrap(~as.factor(b3)+as.factor(a1))+
-         theme_bw(), 
-       width=300, height=300,units='mm', dpi=300)
+# ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParameters.jpg",
+      ggplot(toplot, aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
+        geom_point()+
+        geom_line()+
+        facet_wrap(~as.factor(b3)+as.factor(a1))+
+        theme_bw()
+      # ,width=300, height=300,units='mm', dpi=300)
 # a3 0.2 and b3 
-ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParameters2.jpg",
+# ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParameters2.jpg",
        ggplot(toplot[toplot$b3 < 0.7 &
                        toplot$a1 < 0.5 &
                        toplot$a3 < 0.3,], aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
          geom_point()+
          geom_line()+
          facet_wrap(~as.factor(b3)+as.factor(a1))+
-         theme_bw(), 
-       width=300, height=300,units='mm', dpi=300)
-head(toplot)
-aggregate(survival ~ a1+a3+b3, sum, data=toplot)
-aggregate(survival ~ a1+a3+b3, min, data=toplot)
+         theme_bw()
+       # ,width=300, height=300,units='mm', dpi=300)
 
+       
+parametersfast <- do.call(rbind, lapply(seq(0,0.19,by=0.01), function(a1){ # a1 < 0.2
+   outa3 <- do.call(rbind, lapply(seq(0,0.29,by=0.03), function(a3){ # a3 < 0.3
+     outb3 <- do.call(rbind, lapply(seq(0,0.59,by=0.05), function(b3){ # b3 < 0.6
+       surv <- survivalTypeIII(alpha1 = a1, 1:4, alpha3 = a3, beta3 = b3)
+       data.frame(a1, a3, b3, stage = c("x1","x2","x3","x4"), survival = surv)#x1 = surv[1], x2 = surv[2], x3 = surv[3], x4 = surv[4])
+     }))
+     outb3
+   }))
+   outa3
+ }))
+ 
+parametersslow <- do.call(rbind, lapply(seq(0.0,0.6,by=0.1), function(a1){ # a1 < 0.2
+   outa3 <- do.call(rbind, lapply(seq(0.05,0.59,by=0.05), function(a3){ # a3 < 0.3
+     outb3 <- do.call(rbind, lapply(seq(0.05,0.29,by=0.05), function(b3){ # b3 < 0.6
+       surv <- survivalTypeIII(alpha1 = a1, c(1,4,10,16), alpha3 = a3, beta3 = b3)
+       data.frame(a1, a3, b3, stage = c("x1","x2","x3","x4"), survival = surv)#x1 = surv[1], x2 = surv[2], x3 = surv[3], x4 = surv[4])
+     }))
+     outb3
+   }))
+   outa3
+ }))
+
+paramlist_slow <- parametersslow[parametersslow$stage == "x4" & parametersslow$survival > 0.01,]
+
+ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParametersslow.jpg",
+       ggplot(parametersslow[interaction(parametersslow[,1:3]) %in% interaction(paramlist_slow[,1:3]),], 
+              aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
+         geom_point()+
+         geom_line()+
+         facet_wrap(~as.factor(b3)+as.factor(a1))+
+         theme_bw() ,width=300, height=300,units='mm', dpi=300)
+
+aggregate(survival ~ stage, min, data = parametersslow[interaction(parametersslow[,1:3]) %in% interaction(paramlist_slow[,1:3]),])
+aggregate(survival ~ stage, max, data = parametersslow[interaction(parametersslow[,1:3]) %in% interaction(paramlist_slow[,1:3]),])
+
+paramlist_fast <- parametersfast[parametersfast$stage == "x4" & parametersfast$survival > 0.2 & !is.na(parametersfast$survival) &
+                                   parametersfast$survival < 0.5,]
+aggregate(survival ~ stage, min, data = parametersfast[interaction(parametersfast[,1:3]) %in% interaction(paramlist_fast[,1:3]),])
+aggregate(survival ~ stage, max, data = parametersfast[interaction(parametersfast[,1:3]) %in% interaction(paramlist_fast[,1:3]),])
+ggsave(filename =  "C:/Users/DePrengm/OneDrive - Denver Botanic Gardens/P drive/My Documents/UCDenver_phd/Dissertation/Chapter3/Figures/SurvivalCurveParametersfast.jpg",
+       ggplot(parametersfast[interaction(parametersfast[,1:3]) %in% interaction(paramlist_fast[,1:3]),], 
+       aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~as.factor(b3)+as.factor(a1))+
+  theme_bw() ,width=300, height=300,units='mm', dpi=300)
 # SLOW
 # which combinations of parameters needed to keep survival of adults above 0.2 - this would be slow, get to adult and stay adult a while
-slowparameters <- do.call(rbind, lapply(seq(0,0.19,by=0.01), function(a1){
-  outa3 <- do.call(rbind, lapply(seq(0,0.29,by=0.03), function(a3){
-    outb3 <- do.call(rbind, lapply(seq(0,0.59,by=0.05), function(b3){
-      surv <- survivalTypeIII(alpha1 = a1, 1:4, alpha3 = a3, beta3 = b3)
-      data.frame(a1, a3, b3, stage = c("x1","x2","x3","x4"), survival = surv)#x1 = surv[1], x2 = surv[2], x3 = surv[3], x4 = surv[4])
-    }))
-    outb3
-  }))
-  outa3
-}))
-paramsSurv <- unique(slowparameters[slowparameters$stage == "x4" & slowparameters$survival > 0.2,c("a1","a3","b3")])
-boxplot(paramsSurv$a1, paramsSurv$a3, paramsSurv$b3,
-        main = "Survival parameters Type III",
-        at = c(1,2,3),
-        names = c("a1","a3","b3"),
-        col = c("orange","red","gold"),
-        horizontal = TRUE,
-        notch = TRUE)
-
-plot(1:3, paramsSurv[2,], type = "l", xaxt='n', xlab = "parameters", ylab = "parameter value combinations",
+paramsSurvSlow <- unique(parameters[parameters$stage == "x4" & parameters$survival > 0.2,c("a1","a3","b3")])
+plot(1:3, paramsSurvSlow[2,], type = "l", xaxt='n', xlab = "parameters", ylab = "parameter value combinations",
      ylim = c(0,0.6))
 axis(1, at = c(1,2,3),labels = c("a1","a3","b3"))
-for(i in 3:nrow(paramsSurv)){
-  lines(1:3, paramsSurv[i,], col = i)
+for(i in 3:nrow(paramsSurvSlow)){
+  lines(1:3, paramsSurvSlow[i,], col = i)
 }
-
-slow <- merge(slowparameters, paramsSurv, by = c("a1","a3","b3"))
+slow <- merge(parameters, paramsSurvSlow, by = c("a1","a3","b3"))
 min(slow$survival[slow$stage == "x1"])
 max(slow$survival[slow$stage == "x1"])
 
-
-
-# range of iteroparaty as in greater survival of older ones when b3 < 0.5 & a1 < 0.4
-ggplot(merge(toplot, paramsSurv, by = c("a1","a3","b3")), aes(stage, survival, colour = as.factor(a3), group = interaction(a1,a3)))+
- geom_point()+
- geom_line()+
- facet_wrap(~as.factor(b3)+as.factor(a1))+
- theme_bw()
- 
 # FAST
 # which combinations of parameters needed to keep survival of adults below 0.2 - this would be fast, get to adult and are likely to die, 
 # lower iteroparity, but should have higher output of seed
-paramsSurv <- unique(toplot[toplot$stage == "x4" & toplot$survival <= 0.2,c("a1","a3","b3")])
+paramsSurvfast <- unique(parameters[parameters$stage == "x4" & parameters$survival <= 0.2,c("a1","a3","b3")])
 # What is the minimum stage 1 can be with these parameters?
+plot(1:3, paramsSurvfast[2,], type = "l", xaxt='n', xlab = "parameters", ylab = "parameter value combinations",
+     ylim = c(0,1))
+axis(1, at = c(1,2,3),labels = c("a1","a3","b3"))
+for(i in 3:nrow(paramsSurvfast)){
+  lines(1:3, paramsSurvfast[i,], col = i, lty = i)
+}
+fast <- merge(parameters, paramsSurvfast, by = c("a1","a3","b3"))
+min(fast$survival[fast$stage == "x1"])
+max(fast$survival[fast$stage == "x1"])
 
 
 # ------------------ Iteroparous fast ------------------------
+# only progressive, no retrogressive growth; r_ij = 0
 MPM_iterofast <- function(){
   # three juvenile stages, one reproductive
-  s_s <- survivalTypeIII(alpha1 = runif(1,0,0.1), 1:4, alpha3 = runif(1, 0,0.2), beta3 = runif(1, 0,0.4))
+  # s_s <- survivalTypeIII(alpha1 = runif(1,0,0.1), 1:4, alpha3 = runif(1, 0,0.2), beta3 = runif(1, 0,0.4))
+  i <- sample(1:nrow(paramsSurvfast),1)
+  s_s <- survivalTypeIII(alpha1 = paramsSurvfast[i,1],1:4, alpha3 = paramsSurvfast[i,2], beta3 = paramsSurvfast[i,3] )
   f <- fecunditysurvival(s_s[4])
-  g_j <- s_s/sum(s_s) # scale to 1, transitions follow same pattern as survival most likely to climb one stage; same for dropping in 
-  r_j <- s_s/sum(s_s) 
-  t_ij <- matrix(c(0, s_s[1]*sum(g_j[1:2]), s_s[1]*g_j[3],       s_s[1]*g_j[4],
-                   0, s_s[2]*sum(g_j[1:2]), s_s[2]*g_j[3],       s_s[2]*g_j[4],
-                   0, 0,                    s_s[3]*sum(g_j[1:3]),s_s[3]*g_j[4],
-                   f, 0,                    0,                   s_s[4]*sum(g_j[1:4])), nrow = 4)
-  lambda1 <- lambda(t_ij)
-  while(lambda1>1.2){ # allow more or less variability by speed of life, more variable for fastest
-    i <- mostelastic <- which(popbio::elasticity(t_ij) == max(popbio::elasticity(t_ij)))
-    while(i == mostelastic){ # What if the most elastic stage changes? 
+  # g_j <- s_s/sum(s_s) # scale to 1, transitions follow same pattern as survival most likely to climb one stage; same for dropping in 
+  # r_j <- s_s/sum(s_s) 
+  # t_ij <- matrix(c(0, s_s[1]*sum(g_j[1:2]), s_s[1]*g_j[3],       s_s[1]*g_j[4],
+  #                  0, s_s[2]*sum(g_j[1:2]), s_s[2]*g_j[3],       s_s[2]*g_j[4],
+  #                  0, 0,                    s_s[3]*sum(g_j[1:3]),s_s[3]*g_j[4],
+  #                  f, 0,                    0,                   s_s[4]*sum(g_j[1:4])), nrow = 4)
+  t_ij <- matrix(c(0, s_s[1]*(1/3), s_s[1]*(1/3),       s_s[1]*(1/3),
+                   0, s_s[2]*(1/3), s_s[2]*(1/3),       s_s[2]*(1/3),
+                   0, 0,                    s_s[3]*(1/2),s_s[3]*(1/2),
+                   f, 0,                    0,                   s_s[4], nrow = 4)
+    lambda1 <- lambda(t_ij)
+  while(lambda1>1.2){ # allow more or less variability by speed of life, more variable for fastest rnorm(1, 1, 0.1) for limits
+  i <- mostelastic <- which(popbio::elasticity(t_ij) == max(popbio::elasticity(t_ij)))
+    while(i == mostelastic){ # What if the most elastic transition changes? 
       for(i in seq(0.1,0.5, by = 0.1)){
         t_ij[mostelastic] <- (1-i) * t_ij[mostelastic]
         lambda1 <- lambda(t_ij)
@@ -182,6 +201,7 @@ MPM_iterofast <- function(){
 }
 
 MPMs_itfast <- lapply(1:100, function(x) MPM_iterofast())
+generation.time(mean(MPMs_itfast))
 hist(unlist(lapply(MPMs_itfast, function(x) lambda(x))))
 
 # ------------------------- just one, cover them all ------------------
@@ -233,7 +253,7 @@ MPM_virtual <- function(speed = "fast", parity = "iteroparous", annual = FALSE){
   return(t_ij)
 }
 
-
+# ----------------------- Notes: ----------------------------------------------------------------
 
 # Calculate Lambda and 
 elast <- popbio::elasticity(t_ij)
@@ -247,6 +267,21 @@ lambda(t_ij*scaledvalues) # dot matrix multiplication basically
 
 
 #-------------------------------------------------------------------------------------------------
+
+
+
+# <https://rushinglab.github.io/WILD3810/articles/lab7_tree_harvest.html> 
+eigen(t_ij)
+# a negative value flips space, inverts space, absolute value is how scaled, first derivative is the 
+# determinant is the volume, columns are linearly dependent if squishes to a plane, line, or point
+# thumb = khat, pointer = ihat, middle = jhat
+det(t_ij) # A - lambda*I where roots of polynomial are the eigenvalues, how much the linear transformation scales the unit area, 
+# This makes it very small, if goes onto a line or point, all regions becomes zero, into a smaller dimension
+solve(t_ij) # The inverse, so multiplying by the matirx and by the inverse gets back to original 
+vec <- matrix(runif(4, 0,1), ncol = 1)
+solve(t_ij) %*% (t_ij %*% vec)
+solve(t_ij) %*% t_ij
+
 
 # imaginay numbers means rotation-scaling matrix. change to eigenbasis, transform matrix to eigenbasis 
 # eigenvectors
